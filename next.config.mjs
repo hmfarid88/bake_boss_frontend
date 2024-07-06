0b9bf4aac874dd94aa996c143e79bfe4a01e1829
hmfarid88 <hmfariduddin189@gmail.com>
2024-07-06 12:16:50 +0600

    next config modified

--- next.config.mjs
+++ next.config.mjs
@@ -1,26 +1,73 @@
-/** @type {import('next').NextConfig} */
+// /** @type {import('next').NextConfig} */
+
+// const nextConfig = {
+//     images:{
+//         remotePatterns:[
+//             {
+//                 protocol:"https",
+//                 hostname:"lh3.googleusercontent.com"
+//             }
+//         ]
+//     },
+//     // experimental: {
+//     //     serverActions: {
+//     //       allowedOrigins: ['http://localhost:8080'],
+//     //     },
+//     //   },
+   
+// };
+
+
+// const withMDX = require('@next/mdx')({
+//     extension: /\.(md|mdx)$/,
+//   });
+//   const withImages = require('next-images');
+//   require('dotenv').config({ path: `./.env.${process.env.ENVIRONMENT}` });
+  
+//   module.exports = withImages(
+//     withMDX({
+//       env: {
+//         SESSION_SECRET: process.env.SESSION_SECRET,
+//         NEXT_PUBLIC_API_BASE_URL: process.env.NEXT_PUBLIC_API_BASE_URL,
+//       },
+//       webpack(config, options) {
+//         return config;
+//       },
+//     })
+//   );
+  
+
+// export default nextConfig;
+
+
+import withMDX from '@next/mdx';
+import dotenv from 'dotenv';
+
+dotenv.config({ path: `./.env.${process.env.ENVIRONMENT}` });
+
+const mdxConfig = withMDX({
+  extension: /\.(md|mdx)$/,
+});
 
 const nextConfig = {
-    images:{
-        remotePatterns:[
-            {
-                protocol:"https",
-                hostname:"lh3.googleusercontent.com"
-            }
-        ]
-    },
-    experimental: {
-        serverActions: {
-          allowedOrigins: ['http://localhost:8080'],
-        },
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'lh3.googleusercontent.com',
       },
-    //   async redirects() {
-    //     return [{
-    //         source: '/',
-    //         destination: '/login',
-    //         permanent: true
-    //     }]
-    // }
+    ],
+  },
+  env: {
+    SESSION_SECRET: process.env.SESSION_SECRET,
+    NEXT_PUBLIC_API_BASE_URL: process.env.NEXT_PUBLIC_API_BASE_URL,
+  },
+  webpack(config, options) {
+    return config;
+  },
+  
 };
 
-export default nextConfig;
+export default mdxConfig(nextConfig);
+
+  
