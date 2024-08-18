a33fd0edefe821390ec1c00d7d81796b55eaae52
hmfarid88 <hmfariduddin189@gmail.com>
2024-08-18 16:15:30 +0600

    update

--- src/app/components/Sales-Sidebar.tsx
+++ src/app/components/Sales-Sidebar.tsx
@@ -20,7 +20,6 @@ export const SalesSidebar = () => {
             <div className="drawer lg:drawer-open z-50">
                 <input id="my-drawer-3" type="checkbox" className="drawer-toggle" />
                 <div className="drawer-content flex flex-col">
-
                     <div className="flex-none lg:hidden">
                         <label htmlFor="my-drawer-3" aria-label="open sidebar" className="btn btn-square btn-ghost">
                             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" className="inline-block w-6 h-6 stroke-current"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16M4 18h16"></path></svg>
