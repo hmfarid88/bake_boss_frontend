f1fcc402e298ca9a7b53597935e86b702380ce34
hmfarid88 <hmfariduddin189@gmail.com>
2025-11-26 13:53:13 +0600

    limited service added

--- src/app/(sales-item)/sales-dashboard/page.tsx
+++ src/app/(sales-item)/sales-dashboard/page.tsx
@@ -28,15 +28,15 @@ export default function Page() {
               </div>
             </div>
           </div>
-          <div className="flex flex-col items-center justify-center">
+          {/* <div className="flex flex-col items-center justify-center">
             <div className="p-5"><h4 className="uppercase font-bold text-sm">Last Six Month Sales Analysis</h4></div>
             <div><Barchart /></div>
-          </div>
+          </div> */}
         </div>
-        <div className="flex flex-col items-center justify-center pb-10">
+        {/* <div className="flex flex-col items-center justify-center pb-10">
           <div className="p-5"><h4 className="uppercase font-bold text-sm">Last 12 Month Profit-Loss Analysis</h4></div>
           <div><Linechart /></div>
-        </div>
+        </div> */}
       </div>
     </main>
   );
