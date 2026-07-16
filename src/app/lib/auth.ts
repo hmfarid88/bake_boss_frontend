b8290cbc1fd3fe2e9b2638e287ee1ed659eee56f
hmfarid88 <hmfariduddin189@gmail.com>
2026-07-17 01:50:49 +0600

    updated

--- src/app/lib/auth.ts
+++ src/app/lib/auth.ts
@@ -30,7 +30,7 @@ export async function createSession(username: string, roles: string) {
 
   cookies().set('aurorafood_session', session, {
     httpOnly: true,
-    secure: true,
+    secure: false,
     expires: expiresAt,
     sameSite: 'lax',
     path: '/',
@@ -48,7 +48,7 @@ export async function updateSession() {
   const expires = new Date(Date.now() + 24 * 60 * 60 * 1000)
   cookies().set('aurorafood_session', session, {
     httpOnly: true,
-    secure: true,
+    secure: false,
     expires: expires,
     sameSite: 'lax',
     path: '/',
